dfca0810f07ed06cf5dff609dd0448b38322a2fc
Ludovico M. Valenti <32045840+Link199292@users.noreply.github.com>
2022-10-24 01:37:11 +0200

    dataset functions

---
+++ DeepLabV3Plus/custom_dataset.py
@@ -0,0 +1,60 @@
+import torch
+import cv2
+from glob import glob
+from utils import one_hot_encode, mask_to_consider
+
+class DamageDataset(torch.utils.data.Dataset):
+    def __init__(self, root_dir, class_rgb_values=None, transform=None, preprocessing=None, train=True):
+        task = 'train' if train else 'test'
+        self.images = glob(f'{root_dir}/{task}/image/*')
+        self.images.sort()
+        self.masks = glob(f'{root_dir}/{task}/mask/*')
+        self.masks.sort()
+        self.preprocessing = preprocessing
+        self.class_rgb_values = class_rgb_values
+        self.transform = transform
+        assert len(self.images) == len(self.masks)
+    
+    def __getitem__(self, idx):
+        image = cv2.cvtColor(cv2.imread(self.images[idx]), cv2.COLOR_BGR2RGB)
+        mask = cv2.cvtColor(cv2.imread(self.masks[idx]), cv2.COLOR_BGR2RGB)
+        
+        mask = one_hot_encode(mask, self.class_rgb_values).astype('float')
+        
+        if self.transform is not None:
+            sample = self.transform(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        if self.preprocessing is not None:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        else:
+            image, mask = image.transpose(2, 0, 1).astype('float32'), mask.transpose(2, 0, 1).astype('float32')
+        
+        return image, mask
+    
+    def __len__(self):
+        return len(self.images)
+    
+    
+class ValidationDataset(torch.utils.data.Dataset):
+    def __init__(self, root_dir, transform=None, preprocessing=None, class_rgb_values=None):
+        self.images = glob(f'{root_dir}/*')
+        self.images.sort()
+        self.preprocessing = preprocessing
+        self.transform = transform
+        self.class_rgb_values = class_rgb_values
+        
+    def __getitem__(self, idx):
+        image = cv2.cvtColor(cv2.imread(self.images[idx]), cv2.COLOR_BGR2RGB)
+        if self.transform is not None:
+            sample = self.transform(image=image)
+            
+        if self.preprocessing is not None:
+            sample = self.preprocessing(image=image)
+            image = sample['image']
+            
+        return image
+    
+    def __len__(self):
+        return len(self.images)
