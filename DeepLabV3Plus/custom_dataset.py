f46864d5a0262a2d35514e1f044879768b8773bf
Ludovico M. Valenti <32045840+Link199292@users.noreply.github.com>
2022-11-03 12:46:05 +0100

    Update custom_dataset.py

--- DeepLabV3Plus/custom_dataset.py
+++ DeepLabV3Plus/custom_dataset.py
@@ -1,10 +1,12 @@
-import torch
-import cv2
+from torch.utils.data import Dataset
 from glob import glob
-from utils import one_hot_encode, mask_to_consider
+import cv2
+from utils import one_hot_encode
+import torch
 
-class DamageDataset(torch.utils.data.Dataset):
-    def __init__(self, root_dir, class_rgb_values=None, transform=None, preprocessing=None, train=True):
+
+class SegmentationDataset(Dataset):
+    def __init__(self, root_dir, class_rgb_values, transform=None, preprocessing=None, train=True):
         task = 'train' if train else 'test'
         self.images = glob(f'{root_dir}/{task}/image/*')
         self.images.sort()
@@ -13,30 +15,31 @@ class DamageDataset(torch.utils.data.Dataset):
         self.preprocessing = preprocessing
         self.class_rgb_values = class_rgb_values
         self.transform = transform
+
         assert len(self.images) == len(self.masks)
-    
+
     def __getitem__(self, idx):
         image = cv2.cvtColor(cv2.imread(self.images[idx]), cv2.COLOR_BGR2RGB)
         mask = cv2.cvtColor(cv2.imread(self.masks[idx]), cv2.COLOR_BGR2RGB)
-        
+
         mask = one_hot_encode(mask, self.class_rgb_values).astype('float')
-        
+
         if self.transform is not None:
             sample = self.transform(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
-        
+
         if self.preprocessing is not None:
             sample = self.preprocessing(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
         else:
             image, mask = image.transpose(2, 0, 1).astype('float32'), mask.transpose(2, 0, 1).astype('float32')
-        
+
         return image, mask
-    
+
     def __len__(self):
         return len(self.images)
-    
-    
+
+
 class ValidationDataset(torch.utils.data.Dataset):
     def __init__(self, root_dir, transform=None, preprocessing=None, class_rgb_values=None):
         self.images = glob(f'{root_dir}/*')
@@ -44,17 +47,21 @@ class ValidationDataset(torch.utils.data.Dataset):
         self.preprocessing = preprocessing
         self.transform = transform
         self.class_rgb_values = class_rgb_values
-        
+
     def __getitem__(self, idx):
         image = cv2.cvtColor(cv2.imread(self.images[idx]), cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (512, 512))
+        #image = Image.open(self.images[idx]).convert('RGB')
         if self.transform is not None:
             sample = self.transform(image=image)
-            
+            image = sample['image']
         if self.preprocessing is not None:
             sample = self.preprocessing(image=image)
             image = sample['image']
-            
+        else:
+            image = image.transpose(2, 0, 1).astype('float32')
+
         return image
-    
+
     def __len__(self):
         return len(self.images)
