8c099cf9db45707764672c36f321486133d2cd09
Ludovico M. Valenti <32045840+Link199292@users.noreply.github.com>
2022-11-03 12:49:22 +0100

    Update validation.py

--- DeepLabV3Plus/validation.py
+++ DeepLabV3Plus/validation.py
@@ -3,48 +3,39 @@ import numpy as np
 import os
 import albumentations as A
 import cv2
-from glob import glob
 from model import load_model
-import segmentation_models_pytorch as smp
 from utils import mask_to_consider, from_segmentation_to_mask, get_preprocessing
-from custom_dataset import ValidationDataset
+from custom_dataset import SegmentationDataset, ValidationDataset
 
+# Specific .pth model to load
+load_from = '/media/link92/E/rifacciamo_segmentation/unet_epochs/epoch_8--trainloss0.15240952546397846--testloss0.15303047994772595--trainiou0.8504173268874486--testiou0.839226317902406.pth'
+# Images for which to predict a mask
+root_dir = '/media/link92/E/caproni_to_predict'
+# Folder where to store predicted masks
+results_folder = '/media/link92/E/caproni_to_predict/predicted'
 
-caproni_dir = '/Public-AI-Challenge-Progetto-Caproni/DeepLabV3Plus/caproni_topredict'
-prev_epochs = '/Public-AI-Challenge-Progetto-Caproni/DeepLabV3Plus/epochs_model'
-results_folder = '/Public-AI-Challenge-Progetto-Caproni/DeepLabV3Plus/caproni_results'
 if not os.path.exists(results_folder):
     os.mkdir(results_folder)
-    
-classes = ['background', 'bands', 'stains', 'scratches', 'dots']
+
+classes = ['background', 'bands', 'stains', 'scratches']
 select_class_rgb_values = mask_to_consider(classes)
 
-DS_MEAN = (0.485, 0.456, 0.406)
-DS_STD = (0.229, 0.224, 0.225)
+DEVICE = torch.device('cpu')
 
-test_transform = A.Compose([
-    A.Resize(height=224, width=224),
-    #A.Normalize(mean=DS_MEAN,
-    #            std=DS_STD)
-])
+model = load_model(DEVICE, n_classes=len(classes), load_from=load_from)
 
-DEVICE = torch.device('cuda')
-
-model = load_model(device=DEVICE, load_best=True, epochs_dir=prev_epochs)
-
-caproni_dataset = ValidationDataset(caproni_dir,
-                                    transform=test_transform,
+# ValidationDataset loads only images (not masks) from the provided root_dir
+caproni_dataset = ValidationDataset(root_dir=root_dir,
+                                    preprocessing=None,
                                     class_rgb_values=select_class_rgb_values)
 
-if __name__ == '__main__':
-    for idx in range(len(caproni_dataset)):
+for idx in range(len(caproni_dataset)):
         image = caproni_dataset[idx]
         x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
-        x_tensor = x_tensor.type(torch.cuda.FloatTensor)    # This further step is required because no preprocessing is done
-        x_tensor = torch.permute(x_tensor, (0, 3, 1, 2))    # and also this
         pred_mask = model(x_tensor)
         pred_mask = pred_mask.detach().squeeze().cpu().numpy()
         pred_mask = np.transpose(pred_mask, (1, 2, 0))
         pred_mask = np.argmax(pred_mask, axis=-1)
         pred_mask = from_segmentation_to_mask(pred_mask)
+        image = np.transpose(image, (1, 2, 0))
         cv2.imwrite(f'{results_folder}/pred_{idx}.png', np.hstack([image, pred_mask]))
